9982983ff8d359fc0cdc58a0df9a05ea9811a806
周粤琦 <zhouyueqi@soulapp.cn>
2023-07-25 21:54:51 +0800

    增加仿探探左右滑功能

---
+++ app/src/main/java/cn/outter/demo/widget/card/ReItemTouchUIUtil.java
@@ -0,0 +1,24 @@
+package cn.outter.demo.widget.card;
+
+import android.graphics.Canvas;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public interface ReItemTouchUIUtil {
+
+     
+    void onDraw(Canvas c, RecyclerView recyclerView, View view,
+                float dX, float dY, int actionState, boolean isCurrentlyActive);
+
+     
+    void onDrawOver(Canvas c, RecyclerView recyclerView, View view,
+                    float dX, float dY, int actionState, boolean isCurrentlyActive);
+
+     
+    void clearView(View view);
+
+     
+    void onSelected(View view);
+}
